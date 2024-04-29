22f79c0d6214937c18306a7dfed35a9043ed10b2
Cristofher <Cristofherrojas.cr@gmail.com>
2024-04-29 16:29:16 -0400

    Avance hasta la clase 29-04

--- Seccion 1/Ejercicio JS/js/script.js
+++ Seccion 1/Ejercicio JS/js/script.js
@@ -1,5 +1,6 @@
 /* Definir las variables a usar */
 let numero = document.getElementById('numero');
+let inMax = document.getElementById('inMax');
 
 function restar() {
     numero.innerText = parseInt(numero.innerText) - 1;
@@ -7,6 +8,15 @@ function restar() {
 }
 
 function sumar() {
-    numero.innerText = parseInt(numero.innerText) + 1;
-    console.log(numero.innerText);
+
+    if (inMax.value == ""){
+        numero.innerText = parseInt(numero.innerText) + 1;
+        console.log(numero.innerText);
+    }else if (parseInt(numero.innerText)<parseInt(inMax.value)){
+        numero.innerText = parseInt(numero.innerText) + 1;
+        console.log(numero.innerText);
+    }else{
+        alert("No se puede superar el valor máximo");
+    }
+
 }
